e5d73a3c142bff4ed4f6e9e89a6d9b54fdc8d401
taterntots <matthew.a.weidner@gmail.com>
2021-10-25 16:08:40 -0400

    successfully hit the github API for serach term tetris. data saving to state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // ----------------------------------------------------------------------------------
 // --------------------------------- DASHBOARD --------------------------------------
 // ----------------------------------------------------------------------------------
 
 function Dashboard() {
+  const [repos, setRepos] = useState([]);
+
+  // UseEffect to hit the github repositories API and set data to state
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json'
+      },
+    }).then(res => {
+      console.log(res)
+      setRepos(res.data.items);
+    })
+  }, [])
+
   return (
     <>
       Dashboard
